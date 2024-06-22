37694fe1ddbc281279c2bbd4ad137beefd8d2bbb
Mateus Meneses <mateusppa10@gmail.com>
2024-06-22 13:27:57 -0300

    :lipstick: Feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
+import Image from "next/image";
+import { LinkButton } from "~/components/ui/Button";
 import Container from "~/components/ui/Container";
 import Heading from "~/components/ui/Heading";
+
+import heroImage from "/public/images/hero.png";
 
 export default function HomePage() {
   return (
     <main>
-      <section>
+      <section className="mb-[1.22rem] mt-8 lg:my-20">
         <Container>
-          <Heading>Heading 1</Heading>
-          <Heading level={2}>Heading 2</Heading>
-          <Heading level={3}>Heading 3</Heading>
-          <p>text xs</p>
-          <p>text sm</p>
-          <p>text base</p>
-          <p>text md</p>
-          <p>text lg</p>
-          <p>text xl</p>
-          <p>text 2xl</p>
+          <Heading className="m-auto mb-4 max-w-[51rem] text-center lg:mb-8">
+            Customer relationship magic.
+          </Heading>
+          <p className="m-auto mb-8 max-w-[34rem] text-center">
+            Powerful, flexible and data-driven, Attio makes it easy to build the
+            exact CRM your business needs.
+          </p>
+          <div className="mb-16 flex justify-center">
+            <LinkButton href="https://attioscrm.vercel.app/signup">
+              Get started
+            </LinkButton>
+          </div>
+          <div className="flex justify-center">
+            <Image src={heroImage} alt="CRM system image" />
+          </div>
         </Container>
       </section>
     </main>
--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -6,23 +6,33 @@ type Props = {
   children: React.ReactNode;
   level?: 1 | 2 | 3 | 4 | 5 | 6;
   style?: 1 | 2 | 3;
+  className?: string;
 };
 
 <h1 className="track"></h1>;
 
-export default function Heading({ children, level = 1, style }: Props) {
+export default function Heading({
+  children,
+  level = 1,
+  style,
+  className,
+}: Props) {
   style = style ?? (level as 1 | 2 | 3);
   return React.createElement(
     `h${level}`,
     {
-      className: cn(`${gilroy.className} font-bold`, {
-        "text-[3.75rem] leading-[3.5rem] tracking-[-0.09375rem] lg:text-[5.75rem] lg:leading-[5.5rem] lg:tracking-[-0.225rem]":
-          style === 1,
-        "text-[2rem] leading-[2.375rem] tracking-[-0.03125rem] lg:text-[3.25rem] lg:leading-[3.625rem] lg:tracking-[-0.075rem]":
-          style === 2,
-        "text-[1.5rem] leading-[1.625rem] tracking-[-0.05rem] lg:text-[2.5rem] lg:leading-[2.75rem] lg:tracking-[0.05rem]":
-          style === 3,
-      }),
+      className: cn(
+        `${gilroy.className} font-bold`,
+        {
+          "text-[3.75rem] leading-[3.5rem] tracking-[-0.09375rem] lg:text-[5.75rem] lg:leading-[5.5rem] lg:tracking-[-0.225rem]":
+            style === 1,
+          "text-[2rem] leading-[2.375rem] tracking-[-0.03125rem] lg:text-[3.25rem] lg:leading-[3.625rem] lg:tracking-[-0.075rem]":
+            style === 2,
+          "text-[1.5rem] leading-[1.625rem] tracking-[-0.05rem] lg:text-[2.5rem] lg:leading-[2.75rem] lg:tracking-[0.05rem]":
+            style === 3,
+        },
+        className,
+      ),
     },
     children,
   );
